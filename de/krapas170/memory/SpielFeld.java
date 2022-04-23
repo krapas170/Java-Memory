c1041b151d1a7ba74803b84ee68c39b29b28f3c6
krapas170 <krapas170@gmail.com>
2022-04-23 14:02:29 +0200

    Update SpielFeld.java
    Felder für die Zeitangabe hinzugefügt. Das Spiel ist nun komplett fertig für Version 1.0.

--- de/krapas170/memory/SpielFeld.java
+++ de/krapas170/memory/SpielFeld.java
@@ -1,6 +1,8 @@
 package de.krapas170.memory;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
@@ -12,7 +14,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class SpielFeld extends JFrame { // dem Spielfeld werden die Objekte hinzugefügt
 
@@ -26,7 +27,8 @@ public class SpielFeld extends JFrame { // dem Spielfeld werden die Objekte hinz
   public Knopf knoepfe[][] = new Knopf[xgroesse][ygroesse];
   GridLayout gitterLayout = new GridLayout(0, xgroesse);
   JPanel panel = new JPanel();
-  JTextField anzeige = new JTextField();
+  JButton anzeige = new JButton();
+  JButton zeitAnzeige = new JButton();
   JButton neustart = new JButton("Neustart");
   JButton beenden = new JButton("Beenden");
 
@@ -99,6 +101,7 @@ public class SpielFeld extends JFrame { // dem Spielfeld werden die Objekte hinz
 
     });
     panel.add(anzeige);
+    panel.add(zeitAnzeige);
     pane.add(panel);
     dieSpielSteuerung = new SpielSteuerung(this);
     Zeitschaltung();
@@ -110,13 +113,21 @@ public class SpielFeld extends JFrame { // dem Spielfeld werden die Objekte hinz
       public void run() {
         zeit1--;
         if (zeit1 <= 10) {
-          if (zeit1 % 2 == 0) {
-            System.out.print(Farben.ANSI_RED_BACKGROUND + Farben.ANSI_WHITE);
-          } else {
-            System.out.print(Farben.ANSI_RESET);
+          if (zeit1 >= 0) {
+            if (zeit1 % 2 == 0) {
+              System.out.print(Farben.ANSI_RED_BACKGROUND + Farben.ANSI_WHITE);
+              zeitAnzeige.setBackground(Color.RED);
+            } else {
+              System.out.print(Farben.ANSI_RESET);
+              zeitAnzeige.setBackground(Color.WHITE);
+            }
           }
         }
         System.out.println("Verbleibende Sekunden: " + zeit1 + Farben.ANSI_RESET);
+        anzeige.setEnabled(false);
+        anzeige.setText("Verbleibend:");
+        zeitAnzeige.setEnabled(false);
+        ZeitText();
         try {
           Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -126,6 +137,24 @@ public class SpielFeld extends JFrame { // dem Spielfeld werden die Objekte hinz
         if (zeit1 == 0) {
           verloren();
         } else {
+        }
+      }
+
+      private void ZeitText() {
+        if (zeit1 > 0) {
+
+          int input1 = zeit1;
+
+          final double scale3600 = 1.0 / 3600;
+          final double scale60 = 1.0 / 60;
+          int hh = (int) (input1 * scale3600);
+          int mm = (int) (input1 * scale60);
+          int ss = input1 - mm * 60 - hh * 3600;
+
+          DecimalFormat format = new DecimalFormat("00");
+
+          zeitAnzeige.setText(format.format(mm) + ":"
+              + format.format(ss));
         }
       }
     };
@@ -147,21 +176,21 @@ public class SpielFeld extends JFrame { // dem Spielfeld werden die Objekte hinz
 
   public void gewonnen() {
 
-    int input = zeit1;
+    int input2 = zeit1;
 
     zeit1 = -1;
 
     final double scale3600 = 1.0 / 3600;
     final double scale60 = 1.0 / 60;
-    int hh = (int) (input * scale3600);
-    int mm = (int) (input * scale60);
-    int ss = input - mm * 60 - hh * 3600;
+    int hh = (int) (input2 * scale3600);
+    int mm = (int) (input2 * scale60);
+    int ss = input2 - mm * 60 - hh * 3600;
 
     DecimalFormat format = new DecimalFormat("00");
 
     ImageIcon icon = new ImageIcon("assets/gewonnen.gif");
     JOptionPane.showMessageDialog(null,
-        "Du hast alle Felder aufgedeckt und hattest noch " + format.format(hh) + ":" + format.format(mm) + ":"
+        "Du hast alle Felder aufgedeckt und hattest noch " + format.format(mm) + ":"
             + format.format(ss) + " übrig!",
         "Geschafft",
         JOptionPane.INFORMATION_MESSAGE, icon);
