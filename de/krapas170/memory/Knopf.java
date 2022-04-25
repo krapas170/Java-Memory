7e49fbef982e2fbba6d8be9a888dcaa082c37f4d
krapas170 <krapas170@gmail.com>
2022-04-25 19:43:29 +0200

    Update Knopf.java
    save space

--- de/krapas170/memory/Knopf.java
+++ de/krapas170/memory/Knopf.java
@@ -3,14 +3,11 @@ package de.krapas170.memory;
 import javax.swing.JButton;
 
 public class Knopf extends JButton {
-
     private int x, y;
 
     public Knopf(int px, int py) {
         x = px;
         y = py;
-        setText(x + "/" + y);
-
     }
 
     public int gibX() {
