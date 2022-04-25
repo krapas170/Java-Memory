20fee4aa56e331d2f3e8a1741f43d1cac453c811
krapas170 <krapas170@gmail.com>
2022-04-25 18:55:58 +0200

    Update Main.java
    Save space

--- de/krapas170/memory/Main.java
+++ de/krapas170/memory/Main.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
-
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -20,27 +19,20 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class Main {
-
     static Menue start = new Menue();
     private static HashMap<String, Object> contents;
 
     public static void main(String[] args) {
-
         checkVersion();
-
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
-
         boolean GameIsVisable = true;
-
         boolean MenueIsActive = start.isActive();
-
         runMenue();
-
         while (MenueIsActive) {
             try {
                 Thread.sleep(100);
@@ -49,12 +41,9 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-
                 SpielFeld gitter = new SpielFeld();
-
                 gitter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 gitter.fuegeAllesZurOberflaecheHinzu(gitter.getContentPane());
                 gitter.pack();
@@ -71,7 +60,6 @@ public class Main {
     }
 
     public static void beiFehlerSchließen(String uberschrift, String meldung) {
-
         ImageIcon icon = new ImageIcon("assets/Fehler.jpg");
         JOptionPane.showMessageDialog(null, meldung, uberschrift,
                 JOptionPane.INFORMATION_MESSAGE, icon);
@@ -79,7 +67,6 @@ public class Main {
     }
 
     public static void beiFehlerFortsetzen(String uberschrift, String meldung) {
-
         ImageIcon icon = new ImageIcon("assets/Fehler.jpg");
         JOptionPane.showMessageDialog(null, meldung, uberschrift,
                 JOptionPane.INFORMATION_MESSAGE, icon);
@@ -90,20 +77,13 @@ public class Main {
     static int Zeit;
 
     public static void checkVersion() {
-
-        // JOptionPane.showInputDialog(null, "Überprüfe auf Updates, bitte habe einen
-        // Moment Geduld.");
-
         String message = "Überprüfe auf Updates, bitte habe einen Moment Geduld.";
-
         final JDialog d = new JDialog();
         d.setTitle("Überprüfe Version");
         d.setSize(400, 150);
-
         JLabel message_label = new JLabel(message.toString(), JLabel.CENTER);
         d.add(message_label);
         d.setVisible(true);
-
         try (BufferedInputStream inputStream = new BufferedInputStream(
                 new URL("https://raw.githubusercontent.com/krapas170/Java-Memory/main/version.json").openStream());
                 FileOutputStream fileOS = new FileOutputStream("version-server.json")) {
@@ -129,16 +109,13 @@ public class Main {
             }
         };
         tr1.start();
-
     }
 
     public static void isTwoJsonEquals() throws IOException {
         Path local = FileSystems.getDefault().getPath("version.json");
         Path server = FileSystems.getDefault().getPath("version-server.json");
-
         List localList = Files.readAllLines(local);
         List serverList = Files.readAllLines(server);
-
         if (localList.equals(serverList) != true) {
             Object meldung = (String) "Anscheinend gibt es eine neuere Version des Spiels.\nSoll sie jetzt heruntergeladen werden?";
             String uberschrift = "Neue Version verfügbar";
@@ -162,13 +139,11 @@ public class Main {
                 }
             } else if (answer == 1) {
                 Files.delete(server);
-
                 System.out.println("Spiel wird mit alter Version fortgesetzt");
             } else if (answer == 2) {
                 Files.delete(server);
                 System.exit(0);
             }
-
         }
         Files.delete(server);
     }
