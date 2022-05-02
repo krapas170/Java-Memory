aa2c8e14a4ce13f9766bbbe67b7997d0650e31c0
krapas170 <krapas170@gmail.com>
2022-05-02 14:05:23 +0200

    Create a.java

---
+++ de/krapas170/memory/player/a.java
@@ -0,0 +1,19 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package jaco.mp3.player;
+
+final class a extends Thread
+{
+    private /* synthetic */ b a;
+    
+    a(final b a) {
+        this.a = a;
+    }
+    
+    @Override
+    public final void run() {
+        this.a.a.skipForward();
+    }
+}
