c54739c7ae57d4be3b4ee9aa0c380959941fddcc
krapas170 <krapas170@gmail.com>
2022-12-28 14:11:42 +0100

    Update Knopf.java

--- de/krapas170/memory/Knopf.java
+++ de/krapas170/memory/Knopf.java
@@ -5,16 +5,16 @@ import javax.swing.JButton;
 public class Knopf extends JButton {
     private int x, y;
 
-    public Knopf(int px, int py) {
+    protected Knopf(int px, int py) {
         x = px;
         y = py;
     }
 
-    public int gibX() {
+    protected int gibX() {
         return x;
     }
 
-    public int gibY() {
+    protected int gibY() {
         return y;
     }
 }
