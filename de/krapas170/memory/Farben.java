e27226537db1191c39f62b39265c83882f9e6b80
krapas170 <krapas170@gmail.com>
2022-12-28 14:11:37 +0100

    Update Farben.java

--- de/krapas170/memory/Farben.java
+++ de/krapas170/memory/Farben.java
@@ -1,22 +1,23 @@
 package de.krapas170.memory;
+
 public class Farben {
-    public static final String ANSI_RESET = "\u001B[0m"; // Alle Farben werden zurückgesetzt
+    protected static final String ANSI_RESET = "\u001B[0m"; // Alle Farben werden zurückgesetzt
     // Schriftfarben
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    protected static final String ANSI_BLACK = "\u001B[30m";
+    protected static final String ANSI_RED = "\u001B[31m";
+    protected static final String ANSI_GREEN = "\u001B[32m";
+    protected static final String ANSI_YELLOW = "\u001B[33m";
+    protected static final String ANSI_BLUE = "\u001B[34m";
+    protected static final String ANSI_PURPLE = "\u001B[35m";
+    protected static final String ANSI_CYAN = "\u001B[36m";
+    protected static final String ANSI_WHITE = "\u001B[37m";
     // Hintergrundfarbe
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    protected static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    protected static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    protected static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    protected static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    protected static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    protected static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    protected static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    protected static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 }
